08775b56c46e6df0fda7c7ebe7fa656837353e58
damionbrown <dbrown0591>
2017-12-19 09:32:09 -0600

    Hello World Servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+
+
+@WebServlet(name = "HelloWorld", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        int counter = 0;
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException {
+            counter += 1;
+            String name = req.getParameter("name");
+            res.getWriter().println("Hello," + name);
+            res.getWriter().println(counter);
 
     }
 }
